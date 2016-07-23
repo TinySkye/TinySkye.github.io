9ca97e7a78d8d8403f56ed3700cb85d4043a389d
TinySkye <tinyguai@163.com>
2016-07-24 00:27:30 +0800

    Site updated: 2016-07-24 00:27:29

--- demo/dlpage/js/index.js
+++ demo/dlpage/js/index.js
@@ -1,33 +1,38 @@
-function setCSS(){
-	var screen_w = document.body.clientWidth;
 
-	$("#head_img").css({
-		"width": screen_w,
-		"height": screen_w/1.63
+	function setCSS(){
+		var screen_w = document.body.clientWidth;
+
+		$("#head_img").css({
+			"width": screen_w,
+			"height": screen_w/1.63
+		});
+
+		$("#description").css("height",screen_w/4.78);
+
+		$("#description p").css("font-size",screen_w/28);
+
+
+		$(".item_bg").css({
+			"width": screen_w/1.17,
+			"height": screen_w/7.35,
+			"font-size": screen_w/22.18
+		});
+
+		$(".item_text").css("font-size",screen_w/28);
+
+
+		$("#dl_btn").css({
+			"width": screen_w/2.7,
+			"height": screen_w/8.3,
+			"font-size": screen_w/23,
+			"border-radius": screen_w/62
+		});
+
+		$("#foot_text").css("font-size",screen_w/28);
+
+		$("#foot").css("height",screen_w/9);
+	};
+
+	$(document).ready(function(){
+		setCSS();
 	});
-
-	$("#description").css("height",screen_w*0.21);
-
-	$("#description p").css("font-size",screen_w/28);
-
-
-	$(".item_bg").css({
-		"width": screen_w/1.17,
-		"height": screen_w/7.35,
-		"font-size": screen_w/22.18
-	});
-
-	$(".item_text").css("font-size",screen_w/28);
-
-
-	$("#dl_btn").css({
-		"width": screen_w/2.7,
-		"height": screen_w/8.3,
-		"font-size": screen_w/23,
-		"border-radius": screen_w/62
-	});
-
-	$("#foot_text").css("font-size",screen_w/28);
-
-	$("#foot").css("height",screen_w/9);
-}
